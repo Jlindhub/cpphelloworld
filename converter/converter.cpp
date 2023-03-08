c8a95600e5cd9b0cf2aff8c1c5795d0e9bc2203f
James Lind <112474995+Jlindhub@users.noreply.github.com>
2023-03-08 13:41:02 +0100

    marcian time misc fixes

--- converter/converter.cpp
+++ converter/converter.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main(int argc, char* argv[])
 {
     cout << "how much time did Marc say it'd take? give me the number and then the time frame." <<endl;
     int marcInput;
-    char timescale[32];
+    int marcScale = 3; //change depending on how fast you feel marc is
+    string timescale;
     while(!(cin >> marcInput >> timescale))
     {
         cin.clear();
         cin.ignore(10000, '\n');
         cout << "invalid input, try again.";
     }
-    
-    cout << "right, so that'l probably take uuuh " << marcInput *3 << " " << timescale << endl;
+    if(marcInput*marcScale != 1 && timescale.back() != 's')
+    {
+        char addS = 's';
+        timescale.push_back(addS);
+    }
+    cout << "right, so that'l probably take uuuh " << marcInput * marcScale << " " << timescale << "." << endl;
     system("pause");
 }
