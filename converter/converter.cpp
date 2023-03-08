1f08214880ff99b9fc6b40f1d0ab3e875616a39b
James Lind <112474995+Jlindhub@users.noreply.github.com>
2023-03-08 14:19:40 +0100

    service update - variance
    float support and variable marc time scaling depending on faster or slower lectures!

--- converter/converter.cpp
+++ converter/converter.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     cout << "how much time did Marc say it'd take? give me the number and then the time frame." <<endl;
-    int marcInput;
-    int marcScale = 3; //change depending on how fast you feel marc is
+    float marcInput;
+    float marcScale = 3; //change depending on how fast you feel marc is
     string timescale;
     while(!(cin >> marcInput >> timescale))
     {
@@ -18,6 +18,21 @@ int main(int argc, char* argv[])
         char addS = 's';
         timescale.push_back(addS);
     }
+    cout << "is marc going a bit [f]aster or [s]lower today, or is it as normal?" <<endl;
+    char marcfast;
+    while(!(cin >> marcfast))
+    {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        cout << "invalid input, try again.";
+    }
+    switch (marcfast)
+    {
+    case 'f':marcScale = 1.5;break;
+    case 's':marcScale = 5;break;
+    default:marcScale = 3;
+    }
+    
     cout << "right, so that'l probably take uuuh " << marcInput * marcScale << " " << timescale << "." << endl;
     system("pause");
 }
